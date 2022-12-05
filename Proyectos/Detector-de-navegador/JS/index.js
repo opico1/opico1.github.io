ab0cd8b501ccf3e9837dac4d1e9ffcfa0ee0b30f
Carlos Martinez <carlosopico2@gmail.com>
2022-12-04 23:14:24 -0600

    Agregando primer proyecto

---
+++ Proyectos/Detector-de-navegador/JS/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    var ua = navigator.userAgent.match(/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i),browser;
+
+    if (navigator.userAgent.match(/Edge/i) || navigator.userAgent.match(/Trident.*rv[ :]*11\./i)){
+        browser = "msie";
+    }
+    else {
+        browser = ua[1].toLowerCase();
+    }
+    $('div.icon.' + browser).addClass("active");
+});
